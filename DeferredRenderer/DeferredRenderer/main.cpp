449d11eeeb20fce8ca9d4b0b2011bbc9ba6096cb
Felipunky <feli20gutierrez@hotmail.com>
2020-07-25 21:14:39 -0500

    First commit, graphics template including glad, GLFW and stb

---
+++ DeferredRenderer/DeferredRenderer/main.cpp
@@ -0,0 +1,18 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION    
+#include "stb_image.h"
+
+#include <iostream>
+
+int main()
+{
+
+	std::cout << "Hello Render Engine!" << std::endl;
+
+}
